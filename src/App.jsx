0714b58b36aca91357594016b0d9853b78b7a38c
Martin-Kaze <manvispa1@gmail.com>
2026-03-02 18:14:53 +0200

    'name change'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/MapComponent.jsx
 import React, { useRef, useEffect } from "react";
 import Nav from "./components/Nav/Nav.jsx"
-import Mapp from "./components/MAP/Map.jsx"
+import Mapp from "./components/Map/Map.jsx"
 
 export default function MapComponent(){
   return (
